020bd582eaa29564cc08c36cf0bd6801995949d4
manish21689 <mkv.lko.up@gmail.com>
2023-06-01 23:14:28 +0530

    passowrd

--- src/components/Password/Password.jsx
+++ src/components/Password/Password.jsx
@@ -1,26 +1,37 @@
-import React, { useState } from 'react';
-import styles from './Password.module.scss'; 
-const Password = ({ value, setValue, className, width, height, placeholder, label }) => {
+import React, { useState } from "react";
+import styles from "./Password.module.scss";
+const Password = ({
+  value,
+  setValue,
+  className,
+  width,
+  height,
+  placeholder,
+  label,
+}) => {
   const [showPassword, setShowPassword] = useState(false);
   const togglePasswordVisibility = () => {
     setShowPassword(!showPassword);
   };
-
   return (
     <div className={`${styles.password} ${className}`}>
       <label>{label}</label>
-      <div className="input-wrapper">
+      <div className={styles.inputwrapper} style={{ position: "relative" }}>
         <input
-          type={showPassword ? 'text' : 'password'}
+          type={showPassword ? "text" : "password"}
           value={value}
           onChange={(e) => setValue(e.target.value)}
           placeholder={placeholder}
-          style={{ width, height }}
+          style={{ width, height, borderRadius: "4px" }}
         />
-        <img style={{width:'60px',height:'60px'}}
-          className="eye-icon"
-          src={showPassword ? '../../assets/eye-open.png' : '../../assets/eye-close.png'}
-          alt={showPassword ? 'Hide Password' : 'Show Password'}
+        <img
+          className={styles.eyeicon}
+          src={
+            showPassword
+              ? require("../../assets/eye-open.png")
+              : require("../../assets/eye-close.png")
+          }
+          alt={showPassword ? "Hide Password" : "Show Password"}
           onClick={togglePasswordVisibility}
         />
       </div>
--- src/pages/User/UserLogin/UserLogin.jsx
+++ src/pages/User/UserLogin/UserLogin.jsx
@@ -1,21 +1,10 @@
-import React ,{useState} from 'react'
-import PassWord from "../../../components/Password/Password"
 const UserLogin = () => {
-  const [password, setPassword] = useState('');
-  return (
-    <div>UserLogin
-<PassWord
-        value={password}
-        setValue={setPassword}
-        className="custom-input"
-        width="200px"
-        height="30px"
-        placeholder="Enter password"
-        label="Password"
-      />
+  
 
-    </div>
-  )
-}
+  return <div>UserLogin
 
-export default UserLogin
+
+  </div>;
+};
+
+export default UserLogin;
