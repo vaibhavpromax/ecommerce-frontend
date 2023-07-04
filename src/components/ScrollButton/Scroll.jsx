1446cde3a08c33f949aa89b8e437b3c9a0217dbe
manish21689 <mkv.lko.up@gmail.com>
2023-07-04 21:34:16 +0530

    testing

--- src/components/ScrollButton/Scroll.jsx
+++ src/components/ScrollButton/Scroll.jsx
@@ -1,26 +1,9 @@
-// import React from 'react';
-// import styles from "./Scroll.module.scss"
-// const ScrollButton = () => {
-//   const handleClick = () => {
-//     window.scrollTo({
-//       top:0,
-//       behavior: 'smooth'
-//     });
-//   };
-
-//   return (
-//     <button onClick={handleClick} className={styles.scrollbutton}>
-//     Down
-//     </button>
-//   );
-// };
-
-// export default ScrollButton;
-
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./Scroll.module.scss";
 
 const ScrollButton = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
   const handleClick = () => {
     window.scrollTo({
       top: 0,
@@ -28,10 +11,28 @@ const ScrollButton = () => {
     });
   };
 
+  const handleScroll = () => {
+    const button = window.pageYOffset || document.documentElement.scrollTop;
+    setIsVisible(button > 200); 
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  console.log(window.pageYOffset);
+
   return (
-    <button onClick={handleClick} className={styles.scrollbutton}>
-      Scroll Up
-    </button>
+    <div>
+      {isVisible && (
+        <button onClick={handleClick} className={styles.scrollbutton}>
+          Scroll Up
+        </button>
+      )}
+    </div>
   );
 };
 
