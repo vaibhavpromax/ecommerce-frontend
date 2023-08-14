bb65a812364722e33656656807893e514ea7d633
vaibhavpromax <singhvaibhav557hr@gmail.com>
2023-08-14 16:27:16 +0530

    admin page started

--- src/components/TopHeader/TopHeader.jsx
+++ src/components/TopHeader/TopHeader.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
-
+import { useNavigate } from "react-router-dom";
 import styles from "./TopHeader.module.scss";
 import { ICONS } from "../../icons";
 import Button from "../Button/Button";
 
 const Navbar = () => {
+  const { navigate } = useNavigate();
   return (
     <div className={styles.navbar}>
       <div className={styles.left}>
@@ -18,7 +19,9 @@ const Navbar = () => {
       <div className={styles.right}>
         <div className={styles.btn}>La box du moment</div>
         <div className={styles.btn}>Nous contacter</div>
-        <div className={styles.btn}>Shop</div>
+        <div onClick={() => navigate("/shop")} className={styles.btn}>
+          Shop
+        </div>
         <div className={styles.btn}>Se connecter</div>
         <Button className={styles.login}>S’enregistrer</Button>
       </div>
